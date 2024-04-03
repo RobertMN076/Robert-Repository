13c2a1265900a5453f42367b1a04fc0f31ec94bf
Giulio Robert <robert.giulio@escolar.ifrn.edu.br>
2024-04-03 11:28:28 -0300

    first commit

---
+++ aula02.py
@@ -0,0 +1,24 @@
+#print('isso é um texto')
+#coisa = 'deu certo'
+
+
+# nome = input('Digite seu nome: ')
+# idade = float(input('Digite sua idade: '))
+
+
+#print ('Seu nome é: ', nome, 'Sua idade é: ', idade)
+
+#print('\n\n{3:05} e {0:08}'.format(4,5,9,14))
+
+
+num1 = int(input('Insira um número:'))
+num2 = int(input('Insira um segundo número:'))
+
+calculos = (num1 + num2, num1*num2, num1-num2, num1/num2)
+
+print('Os resultados são:',[calculos])
+
+resultados = (num1 > num2, num1 <= num2, num1 != num2, num1 == num2)
+
+print('Os resultados das relações entre eles são:',[resultados])
+
